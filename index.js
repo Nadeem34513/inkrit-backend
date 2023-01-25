826e2586be7d2361a0b008a4a8682921bc98d2c8
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2023-01-25 11:21:21 +0530

    updates

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get("/contact", (req, res) => {
   console.log("Just got a request!");
   res.send({ msg: "hello" });
 });
+app.post("/contact", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send({ data });
+});
+
 app.listen(process.env.PORT || 3000);
