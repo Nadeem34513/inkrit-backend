7c33d5b652fc3b7684b34b5631b9213847237975
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2023-01-25 23:06:31 +0530

    nodemailer test

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const multiparty = require("multiparty");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
+// middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(bodyParser());
@@ -14,6 +18,7 @@ app.get("/contact", (req, res) => {
   res.send({ msg: "hello" });
 });
 
+// routes
 app.post("/contact", (req, res) => {
   console.log(req);
   const data = req.body;
@@ -21,4 +26,53 @@ app.post("/contact", (req, res) => {
   res.send(data);
 });
 
-app.listen(process.env.PORT || 3000);
+var transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "0a71a32d9c99a2",
+    pass: "233a6e4220243d",
+  },
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+app.post("/send", (req, res) => {
+  let form = new multiparty.Form();
+  let data = {};
+  form.parse(req, function (err, fields) {
+    console.log(fields);
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString();
+    });
+
+    //2. You can configure the object however you want
+    const mail = {
+      from: "test 123",
+      to: "inadeemnoushad@gmail.com",
+      subject: data.subject,
+      text: `${data.name} <${data.email}> \n${data.message}`,
+    };
+
+    //3.
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
+  });
+});
+
+// PORT
+const PORT = process.env.PORT || 3000;
+app.listen(() => console.log(`Server running on PORT ${PORT}`));
