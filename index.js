760a028021d5575110f9a62ccdd5710bc2306b7d
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2023-01-25 11:24:14 +0530

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/contact", (req, res) => {
   console.log("Just got a request!");
   res.send({ msg: "hello" });
