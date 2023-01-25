a19fb05bbd1d1c28c11b47012d6b4bd5036a0766
Nadeem Noushad <nadeembinnoushad12@gmail.com>
2023-01-25 11:30:28 +0530

    working

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(bodyParser());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
@@ -10,6 +15,7 @@ app.get("/contact", (req, res) => {
 });
 
 app.post("/contact", (req, res) => {
+  console.log(req);
   const data = req.body;
   console.log(data);
   res.send(data);
